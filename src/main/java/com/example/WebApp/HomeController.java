4b1aa49582aed85782b98e4d6103363814cb83ba
0xffff08 <vls4@discente.ifpe.edu.br>
2025-12-19 22:43:31 +0000

    Finaliza o merge com hotfix e resolve conflitos

--- src/main/java/com/example/WebApp/HomeController.java
+++ src/main/java/com/example/WebApp/HomeController.java
@@ -21,11 +21,11 @@ public class HomeController {
 	public @ResponseBody String greetingIntellij() {
 		return "Hello, Injellij!";
 	}
-
 	@RequestMapping("/hotfix")
 	public @ResponseBody String greetingHotfix() {
-		return "Hello, Hotfix!";
+
+		return "Hello Hotfix!";
 	}
 	
-	
+
 }
